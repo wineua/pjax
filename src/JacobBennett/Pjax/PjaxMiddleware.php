cf459a2eac930d8cd0342a8b1e5711bfb9a45677
Nat Zimmermann <nat@natzim.me>
2015-11-19 13:21:01 +0000

    Update formatting for PSR-2

--- src/JacobBennett/Pjax/PjaxMiddleware.php
+++ src/JacobBennett/Pjax/PjaxMiddleware.php
@@ -1,55 +1,51 @@
-<?php namespace JacobBennett\Pjax;
+<?php
+
+namespace JacobBennett\Pjax;
 
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Symfony\Component\DomCrawler\Crawler;
 
-class PjaxMiddleware {
-
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
+class PjaxMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
         /** @var $response Response */
         $response = $next($request);
 
-        // Only handle non-redirections
-        if (!$response->isRedirection()) {
-            // Must be a pjax-request
-            if ($request->pjax()) {
-                $crawler = new Crawler($response->getContent());
+        // Only handle non-redirections and must be a pjax-request
+        if (!$response->isRedirection() && $request->pjax()) {
 
-                // Filter to title (in order to update the browser title bar)
-                $response_title = $crawler->filter('head > title');
+            // Filter to title (in order to update the browser title bar)
+            $response_title = $crawler->filter('head > title');
 
-                // Filter to given container
-                $response_container = $crawler->filter($request->header('X-PJAX-CONTAINER'));
+            // Filter to given container
+            $response_container = $crawler->filter($request->header('X-PJAX-CONTAINER'));
 
-                // Container must exist
-                if ($response_container->count() != 0) {
-
-                    $title = '';
-                    // If a title-attribute exists
-                    if ($response_title->count() != 0) {
-                        $title = '<title>' . $response_title->html() . '</title>';
-                    }
-
-                    // Set new content for the response
-                    $response->setContent($title . $response_container->html());
+            // Container must exist
+            if ($response_container->count() != 0) {
+                $title = '';
+                // If a title-attribute exists
+                if ($response_title->count() != 0) {
+                    $title = '<title>' . $response_title->html() . '</title>';
                 }
 
-                // Updating address bar with the last URL in case there were redirects
-                $response->header('X-PJAX-URL', $request->getRequestUri());
+                // Set new content for the response
+                $response->setContent($title . $response_container->html());
             }
+
+            // Updating address bar with the last URL in case there were redirects
+            $response->header('X-PJAX-URL', $request->getRequestUri());
         }
 
         return $response;
-	}
-
+    }
 }
